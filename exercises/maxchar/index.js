f1ea06b3f3ecf87d0f75c8ec9084262a7485d95e
Kaitlyn Martinez <kkmartinez95@gmail.com>
2020-04-16 22:21:39 -0400

    day two

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,30 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// APRIL 16th 2020
+
+// Literally the same solution!! yay!!
+
+function maxChar(str) {
+  let memo = {};
+  let num = 0;
+  let letter;
+
+  for (let i = 0; i < str.length; i++) {
+    if (!memo[str[i]]) {
+      memo[str[i]] = 1;
+    } else {
+      memo[str[i]]++;
+    }
+  }
+
+  for (let key in memo) {
+    if (memo[key] > num) {
+      num = memo[key];
+      letter = key;
+    }
+  }
+  return letter;
+}
 
 module.exports = maxChar;
