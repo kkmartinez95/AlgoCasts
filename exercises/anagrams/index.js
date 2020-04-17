5a4a05f4c086e0e91ed3bdaa0d48fd1db8b77202
Kaitlyn Martinez <kkmartinez95@gmail.com>
2020-04-17 00:10:42 -0400

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,40 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// APRIL 16th 2020
+
+function anagrams(stringA, stringB) {
+  // solution code had a helper method to remove spaces/punctuation and to make the hash map...but this works well too.
+  stringA = stringA.replace(/[^\w]/g, "").toLowerCase();
+  stringB = stringB.replace(/[^\w]/g, "").toLowerCase();
+
+  let memoA = {};
+  let memoB = {};
+
+  if (stringA.length !== stringB.length) {
+    return false;
+  }
+
+  for (let i = 0; i < stringA.length; i++) {
+    if (!memoA[stringA[i]]) {
+      memoA[stringA[i]] = 1;
+    } else {
+      memoA[stringA[i]]++;
+    }
+  }
+
+  for (let i = 0; i < stringB.length; i++) {
+    if (!memoB[stringB[i]]) {
+      memoB[stringB[i]] = 1;
+    } else {
+      memoB[stringB[i]]++;
+    }
+  }
+
+  for (let key in memoA) {
+    if (memoA[key] !== memoB[key]) return false;
+  }
+  return true;
+}
 
 module.exports = anagrams;
